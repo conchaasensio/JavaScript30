3a79fa9647ea392c556b887333e976f57f9bdccd
Concha Asensio <conchaasensiomr@gmail.com>
2020-09-14 21:33:15 +0200

    add function to remove transition

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -2,10 +2,19 @@
 
 function playSound(ev) {
   const audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`);
-  const key = document.querySelector(`key[data-key]`);
+  const key = document.querySelector(`div[data-key="${ev.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
   audio.play();
-  keys.classList.add('playing');
 }
 
-const keys = document.querySelectorAll('.key');
+function removeTransition(ev) {
+  if (ev.propertyName !== 'transform') return;
+  ev.target.classList.remove('playing');
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
 window.addEventListener('keydown', playSound);
+keys.forEach((key) => key.addEventListener('transitioned', removeTransition));
